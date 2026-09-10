9f1a42de0e40188f1cdfaed08a5b56e93c0c32e2
rachit74 <rachithooda09@gmail.com>
2026-09-10 20:40:47 +0530

    setup and basic styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h3> Leading Scientific Equipment & Chemical Distributor</h3>
+      <p>Serving the scientific community since January 2014 with premium laboratory equipment, chemicals, and instruments from world-renowned manufacturers and international partners.</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return(
+        <header className="header">
+            <div className="logo">Deep Scientific</div>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Products</a>
+                <a href="">Our Partners</a>
+                <a href="">Contact Us</a>
+                <a href="">Login</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
